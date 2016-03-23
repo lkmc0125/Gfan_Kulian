46be8f65b2e54f075e47f15d38c6306c51cd8ef9
491055741 <donotcrazy@163.com>
2016-03-23 20:15:34 +0800

    network status

--- src/com/xiaohong/kulian/ui/ConnectionActivity.java
+++ src/com/xiaohong/kulian/ui/ConnectionActivity.java
@@ -1,31 +1,34 @@
 package com.xiaohong.kulian.ui;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.xiaohong.kulian.R;
 import com.xiaohong.kulian.common.util.TopBar;
+import com.xiaohong.kulian.common.util.Utils;
 import com.xiaohong.kulian.common.util.WifiAuthentication;
 import com.xiaohong.kulian.common.widget.BaseActivity;
 
 public class ConnectionActivity extends BaseActivity {
     private static final String TAG = "ConnectionActivity"; 
+    private Button mAuthBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connection);
         initTopBar();
-
-        Button authBtn = (Button) findViewById(R.id.authenticationBtn);
-        authBtn.setOnClickListener(new View.OnClickListener() {
-
+        mAuthBtn = (Button) findViewById(R.id.authenticationBtn);
+        mAuthBtn.setVisibility(View.INVISIBLE);
+        mAuthBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 WifiAuthentication auth = new WifiAuthentication();
                 auth.appAuth();
             }
         });
+        checkNetwork();
     }
 
     private void initTopBar() {
@@ -33,5 +36,16 @@ public class ConnectionActivity extends BaseActivity {
                 new View[] { findViewById(R.id.top_bar_title) },
                 new int[] { View.VISIBLE },
                 getString(R.string.connection_title));
+    }
+
+    private void checkNetwork () {
+        String url = "http://115.159.3.16/cb/app_test"; // todo : add mobile para
+        if (null == Utils.httpGet(url)) {
+            Log.d(TAG, "network not availabel");
+            mAuthBtn.setVisibility(View.VISIBLE);
+        } else {
+            Log.d(TAG, "network OK！");
+            mAuthBtn.setVisibility(View.INVISIBLE);
+        }
     }
 }
