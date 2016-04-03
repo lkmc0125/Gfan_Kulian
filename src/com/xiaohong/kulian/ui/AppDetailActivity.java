635d46842794b604d47f6982f2d3b2f72a7b1218
free555 <fredhurui@qq.com>
2016-04-03 18:06:11 +0800

    fix download button cannot work issue

--- src/com/xiaohong/kulian/ui/AppDetailActivity.java
+++ src/com/xiaohong/kulian/ui/AppDetailActivity.java
@@ -49,6 +49,33 @@ public class AppDetailActivity extends Activity
     private static final String TEXT_OPEN = "打开";
     private static final String TEXT_INSTALL = "安装";
     
+    /**
+     * 已安装等待打开
+     */
+    private static final int STATUS_WAITING_OPEN = 1;
+    
+    /**下载完毕等待安装
+     */
+    private static final int STATUS_WAITING_INSTALL = 2;
+    
+    /**
+     * 从未下载过等待开始下载
+     */
+    private static final int STATUS_WAITING_DOWNLOAD = 3;
+    
+    /**
+     * 下载中
+     */
+    private static final int STATUS_DOWNLOADING = 4;
+    
+    /**
+     * 暂停中
+     */
+    private static final int STATUS_PAUSE = 5;
+    
+    private int mStatus;
+    private String mFilePath;
+    
     private int mCoinNum = 0;
     private Session mSession;
 
@@ -117,8 +144,10 @@ public class AppDetailActivity extends Activity
         mRootLayout = (LinearLayout) findViewById(R.id.app_detail_root_layout);
         mRootLayout.setVisibility(View.INVISIBLE);
 
-        mAppActionView.setOnClickListener(null);
+        mAppActionView.setOnClickListener(this);
         mBackImageView.setOnClickListener(this);
+        mAppActionLayout.setOnClickListener(this);
+        mAppActionIv.setOnClickListener(this);
     }
 
     @Override
@@ -175,10 +204,14 @@ public class AppDetailActivity extends Activity
             if (Utils.isApkInstalled(getApplicationContext(),
                     mDetailInfo.getPackagename())) {
                 // 已安装 显示打开
+                mStatus = STATUS_WAITING_OPEN;
                 showOpenView();
             } else if (new File(
                     com.xiaohong.kulian.common.download.Constants.DEFAULT_MARKET_SUBDIR
                             + "/" + mDetailInfo.getAppname() + ".apk").exists()) {
+                mStatus = STATUS_WAITING_INSTALL;
+                mFilePath = com.xiaohong.kulian.common.download.Constants.DEFAULT_MARKET_SUBDIR
+                        + "/" + mDetailInfo.getAppname() + ".apk";
                 showInstallView();
 
             } else if (mSession.getDownloadingList().get(
@@ -195,14 +228,19 @@ public class AppDetailActivity extends Activity
                 Log.d(TAG, "mDownloadId-" + mDownloadId);
                 if (downloadInfo.mFilePath != null
                         && !downloadInfo.mFilePath.equals("")) {
+                    mStatus = STATUS_WAITING_INSTALL;
+                    mFilePath = downloadInfo.mFilePath;
                     showInstallView();
                 } else if (mIsDownloading == true) {
+                    mStatus = STATUS_DOWNLOADING;
                     showDowloadingView(downloadInfo);
                 } else {
+                    mStatus = STATUS_PAUSE;
                     showContinueView(downloadInfo);
                 }
             } else {
                 // do download
+                mStatus = STATUS_WAITING_DOWNLOAD;
                 showDownloadView();
 
             }
@@ -267,6 +305,8 @@ public class AppDetailActivity extends Activity
                 finish();
                 break;
             case R.id.app_action_layout :
+            case R.id.app_action_iv:
+            case R.id.app_action_tv:
                 handleActionTvClicked();
                 break;
 
@@ -309,28 +349,33 @@ public class AppDetailActivity extends Activity
         String tvStr = (String) mAppActionView.getText();
         DownloadInfo downloadInfo = mSession.getDownloadingList().get(
                 mDetailInfo.getPackagename());
-        if(TEXT_INSTALL.equals(tvStr)) {
+        Log.d(TAG, "handleActionTvClicked mStatus = " + mStatus);
+        if(mStatus == STATUS_WAITING_INSTALL) {
             //安装
-        }else if(TEXT_OPEN.equals(tvStr)) {
+            Utils.installApk(getApplicationContext(), new File(mFilePath));
+        }else if(mStatus == STATUS_WAITING_OPEN) {
             //打开
             Utils.openApkByPackageName(getApplicationContext(),
                     mDetailInfo.getPackagename());
-        }else if(TEXT_CONTINUE.equals(tvStr)) {
+        }else if(mStatus == STATUS_PAUSE) {
             //继续
             Log.d(TAG, "continue downloading");
             mIsDownloading = true;
+            mStatus = STATUS_DOWNLOADING;
             showDowloadingView(downloadInfo);
             mSession.getDownloadManager().resumeDownload(mDownloadId);
             
-        }else if(tvStr != null&& tvStr.contains("正在下载")) {
+        }else if(mStatus == STATUS_DOWNLOADING) {
             //暂停
             mIsDownloading = false;
+            mStatus = STATUS_PAUSE;
             showContinueView(downloadInfo);
             mSession.getDownloadManager().pauseDownload(mDownloadId);
             
         }else {
             //点击后开始下载
             mIsDownloading = true;
+            mStatus = STATUS_DOWNLOADING;
             showDowloadingView(downloadInfo);
             mDownloadId = startDownload(mDetailInfo);
             
@@ -447,6 +492,10 @@ public class AppDetailActivity extends Activity
      * 正在下载，会不断更新进度条
      */
     private void showDowloadingView(DownloadInfo downloadInfo) {
+        if(downloadInfo == null) {
+            Log.w(TAG, "showDowloadingView downloadInfo is null");
+            return;
+        }
         String progress = downloadInfo.mProgress == null ? "0%" : downloadInfo.mProgress;
         mAppActionView.setText("正在下载（" + progress + "）");
         mAppActionView.setTextColor(Color.BLUE);
@@ -469,6 +518,10 @@ public class AppDetailActivity extends Activity
      * 下载被暂停，显示继续
      */
     private void showContinueView(DownloadInfo downloadInfo) {
+        if(downloadInfo == null) {
+            Log.w(TAG, "showContinueView downloadInfo is null");
+            return;
+        }
         mAppActionView.setText(TEXT_CONTINUE);
         if(progressStr2Int(downloadInfo.mProgress) < 50) {
             mAppActionView.setTextColor(getResources().
