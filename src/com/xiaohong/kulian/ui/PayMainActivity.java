ae22535df1d208f12acc4be2d57c613dcf02dded
free555 <fredhurui@qq.com>
2016-04-05 22:01:28 +0800

    move io operation into a background thread when do wechat pay

--- src/com/xiaohong/kulian/ui/PayMainActivity.java
+++ src/com/xiaohong/kulian/ui/PayMainActivity.java
@@ -1,7 +1,9 @@
 package com.xiaohong.kulian.ui;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -21,21 +23,21 @@ import com.xiaohong.kulian.common.util.TopBar;
 import com.xiaohong.kulian.common.util.Utils;
 import com.xiaohong.kulian.common.widget.BaseActivity;
 
-public class PayMainActivity extends BaseActivity implements OnClickListener{
+public class PayMainActivity extends BaseActivity implements OnClickListener {
     private IWXAPI mWxApi;
     private static final String TAG = "PayMainActivity";
     private Button mPayBtn;
     private EditText mCoinNumEt;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pay_main);
-        //initTopBar();
+        // initTopBar();
         initData();
         initViews();
     }
-    
+
     private void initViews() {
         mPayBtn = (Button) findViewById(R.id.appay_btn);
         mCoinNumEt = (EditText) findViewById(R.id.coin_num_et);
@@ -47,15 +49,15 @@ public class PayMainActivity extends BaseActivity implements OnClickListener{
         mWxApi.registerApp(Constants.APP_ID);
 
         boolean isPaySupported = mWxApi.getWXAppSupportAPI() >= Build.PAY_SUPPORTED_SDK_INT;
-        Toast.makeText(PayMainActivity.this, String.valueOf(isPaySupported), Toast.LENGTH_SHORT).show();
+        Toast.makeText(PayMainActivity.this, String.valueOf(isPaySupported),
+                Toast.LENGTH_SHORT).show();
     }
 
     void initTopBar() {
-        TopBar.createTopBar(this, 
-                new View[] { findViewById(R.id.back_btn), findViewById(R.id.top_bar_title) },
-                new int[] { View.VISIBLE, View.VISIBLE}, 
-                getString(R.string.feedback_title));
-        ImageButton back = (ImageButton)findViewById(R.id.back_btn);
+        TopBar.createTopBar(this, new View[]{findViewById(R.id.back_btn),
+                findViewById(R.id.top_bar_title)}, new int[]{View.VISIBLE,
+                View.VISIBLE}, getString(R.string.feedback_title));
+        ImageButton back = (ImageButton) findViewById(R.id.back_btn);
         back.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View arg0) {
@@ -67,55 +69,88 @@ public class PayMainActivity extends BaseActivity implements OnClickListener{
     @Override
     public void onClick(View v) {
         int id = v.getId();
-        switch(id) {
-            case R.id.appay_btn:
-                if(mCoinNumEt.getText().toString().equals("")) {
-                    Toast.makeText(getApplicationContext(), 
-                            "亲，请输入要购买的金币数目", 
+        switch (id) {
+            case R.id.appay_btn :
+                if (mCoinNumEt.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(), "亲，请输入要购买的金币数目",
                             Toast.LENGTH_SHORT).show();
                     Log.d(TAG, "no input data");
                     return;
-                }else {
+                } else {
                     doPay();
                 }
                 break;
         }
-        
+
     }
-    
+
     private void doPay() {
-        String url = "http://wxpay.weixin.qq.com/pub_v2/app/app_pay.php?plat=android";
-        Toast.makeText(PayMainActivity.this, "请稍候...", Toast.LENGTH_SHORT).show();
-        try {
-            String ret = Utils.httpGet(url);
-            if (ret != null) {
-                Log.e("get server pay params:", ret);
-                JSONObject json = new JSONObject(ret); 
-                if (null != json && !json.has("retcode") ){
-                    PayReq req = new PayReq();
-                    req.appId           = json.getString("appid");
-                    req.partnerId       = json.getString("partnerid");
-                    req.prepayId        = json.getString("prepayid");
-                    req.nonceStr        = json.getString("noncestr");
-                    req.timeStamp       = json.getString("timestamp");
-                    req.packageValue    = json.getString("package");
-                    req.sign            = json.getString("sign");
-                    req.extData         = "app data"; // optional
-                    Toast.makeText(PayMainActivity.this, "正在跳转到微信", Toast.LENGTH_SHORT).show();
-                    mWxApi.sendReq(req);
-                }else{
-                    Log.d("PAY_GET", "获取支付信息失败："+json.getString("retmsg"));
-                    Toast.makeText(PayMainActivity.this, "错误信息："+json.getString("retmsg"), Toast.LENGTH_SHORT).show();
-                }
-            }else{
-                Log.d("PAY_GET", "服务器连接失败");
-                Toast.makeText(PayMainActivity.this, "无法连接到服务器，请稍后再试", Toast.LENGTH_SHORT).show();
+        final String url = "http://wxpay.weixin.qq.com/pub_v2/app/app_pay.php?plat=android";
+        Toast.makeText(PayMainActivity.this, "请稍候...", Toast.LENGTH_SHORT)
+                .show();
+        new AsyncTask<Void, Void, Void>() {
+
+            protected void onPreExecute() {
+                mPayBtn.setEnabled(false);
             }
-        }catch(Exception e){
-            Log.e("PAY_GET", "异常信息："+e.getMessage());
-            Toast.makeText(PayMainActivity.this, "异常："+e.getMessage(), Toast.LENGTH_SHORT).show();
-        }
-        mPayBtn.setEnabled(true);
+
+            @Override
+            protected Void doInBackground(Void... params) {
+                String ret = Utils.httpGet(url);
+                if (ret != null) {
+                    Log.e(TAG, "get server pay params:" + ret);
+                    JSONObject json = null;
+                    try {
+                        json = new JSONObject(ret);
+                    } catch (JSONException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                    if (null != json && !json.has("retcode")) {
+                        PayReq req = new PayReq();
+                        try {
+                            req.appId = json.getString("appid");
+                            req.partnerId = json.getString("partnerid");
+                            req.prepayId = json.getString("prepayid");
+                            req.nonceStr = json.getString("noncestr");
+                            req.timeStamp = json.getString("timestamp");
+                            req.packageValue = json.getString("package");
+                            req.sign = json.getString("sign");
+                            req.extData = "app data"; // optional
+                            // Toast.makeText(PayMainActivity.this, "正在跳转到微信",
+                            // Toast.LENGTH_SHORT).show();
+                            mWxApi.sendReq(req);
+                        } catch (JSONException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+
+                    } else {
+                        try {
+                            Log.d(TAG, "获取支付信息失败：" + json.getString("retmsg"));
+                            // Toast.makeText(PayMainActivity.this,
+                            // "错误信息："+json.getString("retmsg"),
+                            // Toast.LENGTH_SHORT).show();
+                        } catch (JSONException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+
+                    }
+                } else {
+                    Log.d(TAG, "服务器连接失败");
+                    // Toast.makeText(PayMainActivity.this, "无法连接到服务器，请稍后再试",
+                    // Toast.LENGTH_SHORT).show();
+                }
+                return null;
+            }
+
+            protected void onPostExecute(Void result) {
+                mPayBtn.setEnabled(true);
+            };
+
+        }.execute();
+
     }
 
 }
