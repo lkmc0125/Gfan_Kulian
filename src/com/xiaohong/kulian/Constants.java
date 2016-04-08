c2de7180f85627aea3af6c9d7fb65299296a1f2f
491055741 <donotcrazy@163.com>
2016-04-08 17:12:01 +0800

    update wx pay app id

--- src/com/xiaohong/kulian/Constants.java
+++ src/com/xiaohong/kulian/Constants.java
@@ -515,5 +515,5 @@ public interface Constants {
     // 社区点击次数
     static final String MENU_CLICK_BBS = "打开社区";
     // 微信支付appid(临时demo)
-    static final String APP_ID = "wxb4ba3c02aa476ea1";
+    static final String APP_ID = "wx97d39186445a0675";
 }
