baeecae72c1c21c77cbff3e02f3236845b1e76d5
free555 <fredhurui@qq.com>
2016-04-09 11:27:53 +0800

    add a BuyCoinActivity

---
+++ src/com/xiaohong/kulian/ui/BuyCoinActivity.java
@@ -0,0 +1,21 @@
+package com.xiaohong.kulian.ui;
+
+import com.xiaohong.kulian.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * 购买金币的页面
+ * @author free
+ *
+ */
+public class BuyCoinActivity extends Activity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buy_coin);
+    }
+
+}
