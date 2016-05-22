1b0b56f5dd04c20ca52a21a8e2fe53b50133a53b
albert_liu <870142289@qq.com>
2016-05-22 21:44:56 +0800

    modify buy_item.xml modify colors.xml modify BuyItemGridViewAdapter.java modify GoodsBean.java finish the buy coin layout ui

--- src/com/xiaohong/kulian/adapter/BuyItemGridViewAdapter.java
+++ src/com/xiaohong/kulian/adapter/BuyItemGridViewAdapter.java
@@ -6,17 +6,18 @@ package com.xiaohong.kulian.adapter;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 
-import com.xiaohong.kulian.R;
-import com.xiaohong.kulian.bean.GoodsBean;
-
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.BaseAdapter;
+import android.widget.GridView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import com.xiaohong.kulian.R;
+import com.xiaohong.kulian.bean.GoodsBean;
 
 /**
  * @author free
@@ -78,8 +79,7 @@ public class BuyItemGridViewAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         ViewHolder holder = null;
         if(convertView == null) {
-            convertView = mInfalter.inflate(R.layout.buy_item, null);
-            
+            convertView = mInfalter.inflate(R.layout.buy_item, null);           
             holder = new ViewHolder();
             holder.mCoinLayout = (LinearLayout) convertView.findViewById(R.id.coinlayout);
             holder.mCoinTv = (TextView) convertView.findViewById(R.id.cointv);
@@ -87,6 +87,9 @@ public class BuyItemGridViewAdapter extends BaseAdapter {
             holder.mMoneyUnitTv = (TextView) convertView.findViewById(R.id.moneyunittv);
             holder.mGiveCoinTv = (TextView) convertView.findViewById(R.id.givecointv);
             holder.mGiveCoinNumTv = (TextView) convertView.findViewById(R.id.givecoinnumtv);
+            //added by albert 2016/5/22
+            holder.moneytv_old = (TextView) convertView.findViewById(R.id.moneytv_old);
+            holder.moneyunittv_old = (TextView) convertView.findViewById(R.id.moneyunittv_old);
             convertView.setTag(holder);
             
         }else {
@@ -99,6 +102,9 @@ public class BuyItemGridViewAdapter extends BaseAdapter {
             holder.mMoneyTv.setTextColor(mContext.getResources().getColor(R.color.buy_coin_item_selected_color));
             holder.mMoneyUnitTv.setTextColor(mContext.getResources().getColor(R.color.buy_coin_item_selected_color));
             holder.mGiveCoinTv.setTextColor(mContext.getResources().getColor(R.color.buy_coin_item_selected_dark_white_color));
+            //added by albert 2016/5/22
+            holder.moneytv_old.setTextColor(mContext.getResources().getColor(R.color.buy_coin_item_selected_color_original));
+            holder.moneyunittv_old.setTextColor(mContext.getResources().getColor(R.color.buy_coin_item_selected_color_original));
         }else {
             holder.mCoinLayout
             .setBackgroundResource(R.drawable.coindefaultbg);
@@ -106,16 +112,34 @@ public class BuyItemGridViewAdapter extends BaseAdapter {
             holder.mMoneyTv.setTextColor(mContext.getResources().getColor(R.color.buy_coin_item_un_selected_blue_color));
             holder.mMoneyUnitTv.setTextColor(mContext.getResources().getColor(R.color.buy_coin_item_un_selected_black_color));
             holder.mGiveCoinTv.setTextColor(mContext.getResources().getColor(R.color.buy_coin_item_un_selected_gray_color));
+            //added by albert 2016/5/22
+            holder.moneytv_old.setTextColor(mContext.getResources().getColor(R.color.buy_coin_item_un_selected_blue_color_original));
+            holder.moneyunittv_old.setTextColor(mContext.getResources().getColor(R.color.buy_coin_item_un_selected_black_color_original));
             
         }
         GoodsBean item = mData.get(position);
+        System.out.println("mData"+mData.get(position).getOriginal_price());
+        System.out.println("mData"+mData.get(position).getPrice());
         holder.mGiveCoinNumTv.setText(item.getGiftCoin() + "");
         holder.mCoinTv.setText(item.getName());
+        if(item.getGiftCoin()==0){
+            holder.mGiveCoinTv.setVisibility(View.GONE);
+            holder.mGiveCoinNumTv.setVisibility(View.GONE);
+        }else{
+            holder.mGiveCoinTv.setVisibility(View.VISIBLE);
+            holder.mGiveCoinNumTv.setVisibility(View.VISIBLE);
+        }
         if (item.getPrice() % 100 == 0) {
             holder.mMoneyTv.setText(item.getPrice()/100+"");
         } else {
             float price = (float)item.getPrice()/100.f;
             holder.mMoneyTv.setText(new DecimalFormat("#0.00").format(price));
+        }
+        if (item.getOriginal_price() % 100 == 0) {
+            holder.moneytv_old.setText(item.getOriginal_price()/100+"");
+        } else {
+            float price = (float)item.getOriginal_price()/100.f;
+            holder.moneytv_old.setText(new DecimalFormat("#0.00").format(price));
         }
         return convertView;
     }
@@ -127,6 +151,8 @@ public class BuyItemGridViewAdapter extends BaseAdapter {
         private TextView mMoneyUnitTv;
         private TextView mGiveCoinTv;//赠送金币文本框需要修改字体颜色
         private TextView mGiveCoinNumTv;//显示赠送的金币数目的文本框
+        private TextView moneytv_old;
+        private TextView moneyunittv_old;
     }
 
 }
